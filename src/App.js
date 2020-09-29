90b732b4e9a1791a4cdf48b8e2b1dfe7591362d2
lusy.volkov <lusy.volkov@gmail.com>
2020-09-29 17:46:46 +0300

    react intro

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 
 class App extends Component {
   getStuff() {
+    return "hi"
   }
 
   getMorningGreeting() {
+    return "goog morning:)"
   }
 
   getEveningGreeting() {
+    return "good evening(:"
   }
 
   showCompany(name, revenue) {
+    return <div id="{name}">{name} makes {revenue} each year</div>
   }
 
   getClassName(temperature) {
-
+    if(temperature<15){
+      return "freezing"
+    } else if(temperature>15 && temperature<30){
+      return "fair"
+    } else {
+      return "hell-scape"
+    }
   }
 
   render() {
@@ -31,35 +41,35 @@ class App extends Component {
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 1</h4 >
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+          <h1>{this.getStuff()}</h1>
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 2</h4>
           <div className="exercise" id="spotcheck-2">
-            {/* your code here */}
+          <div>{new Date().getHours()>12 ? this.getEveningGreeting() : this.getMorningGreeting()}</div>
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 3</h4>
           <div className="exercise" id="spotcheck-3">
-            {/* your code here */}
+          {[this.getMorningGreeting(), this.getEveningGreeting(), <p>some text in it</p>]}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+           {companies.map(element => this.showCompany(element.name, element.revenue))}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 2</h4>
           <div className="exercise" id="ex-2">
-            {/* your code here */}
+            <div id="weatherBox" class={this.getClassName(10)}></div>
           </div>
         </div>
       </div>
